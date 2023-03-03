adb03a4774bf389eec263ada675002e6b61e3d35
assigin <117591072+assigin@users.noreply.github.com>
2023-03-03 21:47:29 +0200

    Update mylib.h

---
+++ mylib.h
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<iomanip>
+#include<string>
+#include <vector>
+#include <limits>
+#include <algorithm>
+#include <cstdio>
+#include <random>
+
+using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::left;
+using std::right;
+using std::setprecision;
+using std::setw;
+using std::string;
+using std::copy;
+using std::fixed;
+using std::sort;
